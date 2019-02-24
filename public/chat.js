07a88c9c3f2512c9790662f373bd9eef001c9733
karimelghazouly <karimelghazouly@gmail.com>
2019-02-25 01:19:26 +0200

    add client functionality for sending and reciving messages

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,23 @@
-var socket = io.connect("http://localhost:8070");
+getelement = function(id)
+{
+    return document.getElementById(id);
+}
+var url = "http://localhost:8070"
+var socket = io.connect(url);
+
+var message = getelement("message");
+var handle = getelement("handle");
+var button = getelement("send");
+var output = getelement("output");
+
+
+button.addEventListener('click',function(){
+    socket.emit('chat', {
+        message:message.value,
+        handle:handle.value
+    });
+});
+
+socket.on('chat',function(data){
+    output.innerHTML += "<p><strong>"+data.handle+":</strong>" +data.message+"</p>";
+})
