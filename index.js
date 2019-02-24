5fe266b0fdaa47c80bb4580d4cb215c1c266b8c8
karimelghazouly <karimelghazouly@gmail.com>
2019-02-25 00:22:14 +0200

    make a server and add socket.io connection

---
+++ index.js
@@ -0,0 +1,17 @@
+express = require('express');
+body_parser= require('body-parser');
+socket = require('socket.io');
+
+
+app = express();
+
+server = app.listen(8070, function(){
+    console.log("app is up");
+})
+
+app.use(express.static('public'));
+socketio = socket(server);
+
+socketio.on('connection',function(socket){
+    console.log('hamadasdassadasdsd done');
+});
