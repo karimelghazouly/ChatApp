6fd648c3210663087c4dd20bb639aaa4db9eb4bf
karimelghazouly <karimelghazouly@gmail.com>
2019-02-28 11:44:37 +0200

    add broadcast messages

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ socket = require('socket.io');
 app = express();
 
 server = app.listen(8070, function(){
-    console.log("app is up");
+    console.log("Listening on port 8070");
 })
 
 app.use(express.static('public'));
 socketio = socket(server);
 
 socketio.on('connection',function(socket){
-    console.log('hamadasdassadasdsd done');
+    console.log('Socket connection started');
     
     socket.on('chat',function(data){
         socketio.sockets.emit('chat',data);
+    });
+
+    socket.on('typing',function(data){
+        socket.broadcast.emit('typing',data);
     });
 });
 
--- public/chat.js
+++ public/chat.js
@@ -9,7 +9,7 @@ var message = getelement("message");
 var handle = getelement("handle");
 var button = getelement("send");
 var output = getelement("output");
-
+var feedback = getelement("feedback");
 
 button.addEventListener('click',function(){
     socket.emit('chat', {
@@ -18,6 +18,16 @@ button.addEventListener('click',function(){
     });
 });
 
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
+});
+
 socket.on('chat',function(data){
     output.innerHTML += "<p><strong>"+data.handle+":</strong>" +data.message+"</p>";
+    feedback.innerHTML = "";
+
+});
+
+socket.on('typing',function(data){
+    feedback.innerHTML = "<p><em>"+data+"is typing .. </em> </p>";
 })
